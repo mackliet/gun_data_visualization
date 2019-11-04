aae5ae48172806495e6984fdd5194f2bac0bf3b6
Kevin Reilly <reilly.w.kevin@gmail.com>
2019-11-04 16:32:44 -0700

    remove unused typecasting

--- test/MigrationPatterns.spec.ts
+++ test/MigrationPatterns.spec.ts
@@ -1,7 +1,4 @@
 import {MigrationPatterns} from "../js/Data/MigrationPatterns";
-import * as d3 from "d3";
-import {Year} from "../js/Data/ImportData";
-// Required for testing locally
 const fs = require('fs');
 
 describe('Test Migration Patterns Class Construct', () => {
@@ -9,7 +6,7 @@ describe('Test Migration Patterns Class Construct', () => {
 
     it('Test Example', async () => {
         try {
-            let data = JSON.parse(fs.readFileSync('data/migration.json')) as Year[];
+            let data = JSON.parse(fs.readFileSync('data/migration.json'));
             const migrationPatterns = new MigrationPatterns(data);
             migrationPatterns.data.forEach((value, key) => {
                 console.log(key);
